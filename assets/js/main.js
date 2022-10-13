73b79e96cbd31a36a06d2bba4eeec68faaedff5f
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-13 15:39:10 +0200

    Stampo a schermo tramite insertAdjacentHTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,27 @@
 // Creiamo un lista della spesa
 const shoppingList = [
     `Pane`,
-    `pasta`,
+    `Pasta`,
     `Latte`,
     `Uova`,
     `Pollo`,
     `Carne`
 ]
+
+const shoppingListElement = document.querySelector(`ul`)
 // Utilizziamo il cicloo while
 let i = 0;
 
-while ( i < shoppingList.length) {
+while (i < shoppingList.length) {
     const item = shoppingList[i];
-    console.log(item);
-   
-    
+    // console.log(item);
+
+
+
+      // stampiamo a schermo tramite insertAdjacentHTML
+    const listElement = `<li>${item}</li>`;
+    shoppingListElement.insertAdjacentHTML(`beforeend`,`<li>${item}</li>`);
+
+
     i++;
 }
